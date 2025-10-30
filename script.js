8ad46322b0842f44a9b24576a4c69f3804fc55a1
Anamonteiro03 <ana.9608068@aluno.mg.gov.br>
2025-10-30 14:35:47 -0300

    Gerenciar Usuário

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+async function cadrastarCardapio(cardapio){
+
+}
+
+async function alterarCardapio(id,atualizarCardapio){
+
+}
+
+async function excluirCardapio(id){
+    
+}
+
+async function buscarCardapio(id){
+
+}
+
+ function exibirTabelaCardapio(cardapios){
+
+ }
+ async function listarCardapio(){
+
+ }
+
+ 
